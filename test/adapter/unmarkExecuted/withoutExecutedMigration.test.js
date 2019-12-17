81e58e373eae94fa182477e917bf39066a0312f3
oleg <oleg.korobenko@gmail.com>
2019-12-17 21:27:44 +0300

    add unmarkExecuted without executed migrations test

---
+++ test/adapter/unmarkExecuted/withoutExecutedMigration.test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const testUtils = require('../../utils');
+
+const test = testUtils.test;
+let adapter;
+const migrationName = '1_someMigration';
+
+test('setup', (assert) => {
+	adapter = testUtils.createAdapter();
+
+	return Promise.resolve()
+		.then(() => adapter.connect())
+		.then(() => testUtils.clearAdapterCollection({adapter}));
+});
+
+test(
+	'adapter unmarkExecuted method without executed migration',
+	(assert) => {
+		return Promise.resolve()
+			.then(() => adapter.unmarkExecuted(migrationName))
+			.then(() => testUtils.getExecutedMigrations({adapter}))
+			.then((names) => {
+				assert.deepEqual(
+					names,
+					[],
+					'should do nothing'
+				);
+			});
+	}
+);
+
+test('teardown', (assert) => {
+	return Promise.resolve()
+		.then(() => testUtils.clearAdapterCollection({adapter}))
+		.then(() => adapter.disconnect());
+});
