9160a9a32c50cfcc21e3870b51199b389d07586c
oleg <oleg.korobenko@gmail.com>
2019-12-20 21:16:06 +0300

    add test for getTemplatePath

---
+++ test/adapter/getTemplatePath/withSuitableParams.test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const pathUtils = require('path');
+const testUtils = require('../../utils');
+
+const test = testUtils.test;
+let adapter;
+
+test('setup', (assert) => {
+	adapter = testUtils.createAdapter();
+
+	assert.end();
+});
+
+test(
+	'adapter getTemplatePath method with suitable params',
+	(assert) => {
+		const templatePath = pathUtils.relative(
+			__dirname,
+			adapter.getTemplatePath()
+		);
+
+		assert.equal(
+			templatePath,
+			'../../../lib/migrationTemplate.js',
+			'should return path to migration template'
+		);
+
+		assert.end();
+	}
+);
